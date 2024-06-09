91e10c36bd4eaa051d0595efe8b4ef2ced3ce804
Felipe Triana <felipetrianago@gmail.com>
2024-06-09 00:40:53 -0500

    Query en formato JPQL donde se usan entities en lugar de tablas

---
+++ src/main/java/com/personal/pizzeria/web/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.personal.pizzeria.web.controller;
+
+import com.personal.pizzeria.persistence.entity.CustomerEntity;
+import com.personal.pizzeria.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+
+    @GetMapping("/phone/{phone}")
+    public ResponseEntity<CustomerEntity> getByPhone(@PathVariable String phone){
+        return ResponseEntity.ok(this.customerService.findByPhone(phone));
+
+    }
+}
